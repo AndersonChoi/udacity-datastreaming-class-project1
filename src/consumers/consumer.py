635f94d971461f9e80047053cdd867bd8129df64
최원영(WonYoung Choi)/Data Infrastructure팀/SKP <circlezero@sk.com>
2019-11-28 15:22:29 +0900

    set python Consumer by avro type. add polling statement

--- src/consumers/consumer.py
+++ src/consumers/consumer.py
@@ -15,13 +15,13 @@ class KafkaConsumer:
     """Defines the base kafka consumer class"""
 
     def __init__(
-        self,
-        topic_name_pattern,
-        message_handler,
-        is_avro=True,
-        offset_earliest=False,
-        sleep_secs=1.0,
-        consume_timeout=0.1,
+            self,
+            topic_name_pattern,
+            message_handler,
+            is_avro=True,
+            offset_earliest=False,
+            sleep_secs=1.0,
+            consume_timeout=0.1,
     ):
         """Creates a consumer object for asynchronous use"""
         self.topic_name_pattern = topic_name_pattern
@@ -30,48 +30,25 @@ class KafkaConsumer:
         self.consume_timeout = consume_timeout
         self.offset_earliest = offset_earliest
 
-        #
-        #
-        # TODO: Configure the broker properties below. Make sure to reference the project README
-        # and use the Host URL for Kafka and Schema Registry!
-        #
-        #
         self.broker_properties = {
-                #
-                # TODO
-                #
+            "bootstrap.servers": "PLAINTEXT://localhost:9092",
+            "group.id": f"{topic_name_pattern}",
+            "default.topic.config": {"auto.offset.reset": "earliest"},
         }
 
-        # TODO: Create the Consumer, using the appropriate type.
         if is_avro is True:
             self.broker_properties["schema.registry.url"] = "http://localhost:8081"
-            #self.consumer = AvroConsumer(...)
+            self.consumer = AvroConsumer(self.broker_properties)
         else:
-            #self.consumer = Consumer(...)
-            pass
+            self.consumer = Consumer(self.broker_properties)
 
-        #
-        #
-        # TODO: Configure the AvroConsumer and subscribe to the topics. Make sure to think about
-        # how the `on_assign` callback should be invoked.
-        #
-        #
-        # self.consumer.subscribe( TODO )
+        self.consumer.subscribe([self.topic_name_pattern], on_assign=self.on_assign)
 
     def on_assign(self, consumer, partitions):
-        """Callback for when topic assignment takes place"""
-        # TODO: If the topic is configured to use `offset_earliest` set the partition offset to
-        # the beginning or earliest
-        logger.info("on_assign is incomplete - skipping")
+        # print("partition assign")
         for partition in partitions:
-            pass
-            #
-            #
-            # TODO
-            #
-            #
-
-        logger.info("partitions assigned for %s", self.topic_name_pattern)
+            if self.offset_earliest is True:
+                partition.offset = confluent_kafka.OFFSET_BEGINNING
         consumer.assign(partitions)
 
     async def consume(self):
@@ -83,22 +60,20 @@ class KafkaConsumer:
             await gen.sleep(self.sleep_secs)
 
     def _consume(self):
-        """Polls for a message. Returns 1 if a message was received, 0 otherwise"""
-        #
-        #
-        # TODO: Poll Kafka for messages. Make sure to handle any errors or exceptions.
-        # Additionally, make sure you return 1 when a message is processed, and 0 when no message
-        # is retrieved.
-        #
-        #
-        logger.info("_consume is incomplete - skipping")
-        return 0
+        message = self.consumer.poll(timeout=self.consume_timeout)
 
+        if message is None:
+            # print("== message is None")
+            return 0
+        elif message.error() is not None:
+            # print("== message is error")
+            return 0
+
+        # print("== key:",message.key())
+        # print("== value:",message.value())
+        self.message_handler(message)
+        return 1
 
     def close(self):
-        """Cleans up any open kafka consumers"""
-        #
-        #
-        # TODO: Cleanup the kafka consumer
-        #
-        #
+        # print("== comsumer close")
+        self.consumer.close()
