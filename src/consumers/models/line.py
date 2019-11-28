9684523e3f7766a83dae80de3131509cb4a1ab4c
최원영(WonYoung Choi)/Data Infrastructure팀/SKP <circlezero@sk.com>
2019-11-28 15:21:33 +0900

    modify if conditions

--- src/consumers/models/line.py
+++ src/consumers/models/line.py
@@ -56,16 +56,15 @@ class Line:
 
     def process_message(self, message):
         """Given a kafka message, extract data"""
-        # TODO: Based on the message topic, call the appropriate handler.
-        if True: # Set the conditional correctly to the stations Faust Table
+        if message.topic() == "org.chicago.cta.stations.table.v1":
             try:
                 value = json.loads(message.value())
                 self._handle_station(value)
             except Exception as e:
                 logger.fatal("bad station? %s, %s", value, e)
-        elif True: # Set the conditional to the arrival topic
+        elif "arrivals" in message.topic():
             self._handle_arrival(message)
-        elif True: # Set the conditional to the KSQL Turnstile Summary Topic
+        elif "TURNSTILE_SUMMARY" in message.topic():
             json_data = json.loads(message.value())
             station_id = json_data.get("STATION_ID")
             station = self.stations.get(station_id)
