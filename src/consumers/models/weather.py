023681697fb2db2ffc644152b0ff8445bdb8bef3
최원영(WonYoung Choi)/Data Infrastructure팀/SKP <circlezero@sk.com>
2019-11-28 15:21:53 +0900

    set Weather Variable from process_message

--- src/consumers/models/weather.py
+++ src/consumers/models/weather.py
@@ -6,18 +6,12 @@ logger = logging.getLogger(__name__)
 
 
 class Weather:
-    """Defines the Weather model"""
 
     def __init__(self):
-        """Creates the weather model"""
         self.temperature = 70.0
         self.status = "sunny"
 
     def process_message(self, message):
-        """Handles incoming weather data"""
-        logger.info("weather process_message is incomplete - skipping")
-        #
-        #
-        # TODO: Process incoming weather messages. Set the temperature and status.
-        #
-        #
+        value = message.value()
+        self.temperature = value["temperature"]
+        self.status = value["status"]
